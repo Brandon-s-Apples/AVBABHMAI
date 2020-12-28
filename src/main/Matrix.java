02228fd4ea082d89ff6d9be6c9c1b580256336e2
Apple123_ <brandontthan2005@gmail.com>
2020-12-27 17:57:05 -0800

    added basic matrix

---
+++ src/main/Matrix.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Matrix {
+
+    double[][] data;
+    final int width, height;
+
+    public Matrix(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+}

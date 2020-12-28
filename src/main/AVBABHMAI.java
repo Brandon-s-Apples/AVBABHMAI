0e9d923a3bc4d2f45a7cde8b032ef9711e7e5963
Apple123_ <brandontthan2005@gmail.com>
2020-12-27 18:14:30 -0800

    added the acronym to main

---
+++ src/main/AVBABHMAI.java
@@ -0,0 +1,19 @@
+package main;
+
+public class AVBABHMAI {
+
+    // A
+    // Very
+    // Bad
+    // Attempt by
+    // Brandon
+    // Han to
+    // Make an
+    // Artificial
+    // Intelligence
+
+    public static void main(String[] args) {
+
+    }
+
+}
